ad307e74de926e82296933c95f4daecad99cfcf8
Rodrigo Braz <rfsbraz@gmail.com>
2023-08-05 13:36:05 +0100

    feat: Add support for skipping collections with watched movies
    chore: refactored tautulli to fetch all available items

--- deleterr.py
+++ deleterr.py
@@ -25,15 +25,14 @@ class Deleterr:
 
         # Setup connections
         self.tautulli = Tautulli(config)
-        self.plex = PlexServer(config.get("plex", "url"), config.get("plex", "token"))
+        self.plex = PlexServer(config.get("plex", "url"), config.get("plex", "token"), timeout=120)
         self.sonarr = {connection['name']: SonarrAPI(connection["url"],connection["api_key"]) for connection in config.config.get("sonarr", [])}
         self.radarr = {connection['name']: RadarrAPI(connection["url"],connection["api_key"]) for connection in config.config.get("radarr", [])}
-
+        self.plex_movie_map = self.plex_show_map = {}
+        self.watched_collections = set()
         self.process_radarr()
 
     def process_radarr(self):
-        movie_activity = self.tautulli.get_last_movie_activity()
-
         for name, radarr in self.radarr.items():
             logger.info("Processing radarr instance: '%s'", name)
             all_movie_data = radarr.get_movie()
@@ -42,6 +41,8 @@ class Deleterr:
 
             for library in self.config.config.get("libraries", []):
                 movies_library = self.plex.library.section("Movies")
+                movie_activity = self.tautulli.get_last_movie_activity(library, movies_library.key)
+
 
                 if library.get("radarr") == name:
                     logger.info("Processing library '%s'", library.get("name"))
@@ -77,27 +78,46 @@ class Deleterr:
     def get_library_config(self, config, show):
         return next((library for library in config.config.get("libraries", []) if library.get("name") == show), None)
     
-
+    def get_plex_item(self, plex_library, title, year, alternate_titles=[]):
+        if f"{title} ({year})" in self.plex_movie_map:
+            return self.plex_movie_map[f"{title} ({year})"]
+        
+        # Plex may pick some different titles sometimes, and since we can't only fetch by title, we need to check all of them
+        for title in [title] + alternate_titles:
+            try:
+                pl_movie = plex_library.get(title, year=year)
+                self.plex_movie_map[f"{title} ({year})"] = pl_movie
+                return pl_movie
+            except NotFound:
+                continue
+                
     def apply_library_rules(self, library_config, plex_library, all_data, activity_data):
         # get the time thresholds from the config
-        last_watched_days = library_config.get('last_watched_days', None)
-        last_added_days = library_config.get('last_added_days', None)
+        last_watched_threshold = library_config.get('last_watched_threshold', None)
+        added_at_threshold = library_config.get('added_at_threshold', None)
+        apply_last_watch_threshold_to_collections = library_config.get('apply_last_watch_threshold_to_collections', False)
         
+        if apply_last_watch_threshold_to_collections:
+            logger.debug(f"Gathering collection watched status")
+            for watched_data in activity_data:
+                plex_movie = self.get_plex_item(plex_library, watched_data['title'], watched_data['year'])
+                if plex_movie is None:
+                    logger.error(f"Movie {watched_data['title']} ({watched_data['year']}) not found in Plex: {watched_data}")
+                    continue
+                last_watched = (datetime.now() - watched_data['last_watched']).days
+                if last_watched_threshold and last_watched < last_watched_threshold:
+                    logger.debug(f"Movie {watched_data['title']} watched {last_watched} days ago, adding collection {plex_movie.collections} to watched collections")
+                    self.watched_collections = self.watched_collections | set([c.tag for c in plex_movie.collections])
         # store the shows that need action
         shows_needing_action = []
 
         for movie_data in all_data:
-
-            for title in [movie_data['title']] + [t['title'] for t in movie_data['alternateTitles']]:
-                try:
-                    plex_movie = plex_library.get(title, year=movie_data['year'])
-                except NotFound:
-                    continue
+            plex_movie = self.get_plex_item(plex_library, movie_data['title'], movie_data['year'], [t['title'] for t in movie_data['alternateTitles']])
             if plex_movie is None:
-                logger.error(f"Movie {movie_data['title']} ({movie_data['year']}) not found in Plex: {movie_data}")
-                exit(1)
+                logger.warning(f"Movie {movie_data['title']} ({movie_data['year']}) not found in Plex, probably a mismatch in the release year metadata")
+                continue
 
-            if not is_movie_actionable(library_config, activity_data, movie_data, plex_movie, last_watched_days, last_added_days):
+            if not self.is_movie_actionable(library_config, activity_data, movie_data, plex_movie, last_watched_threshold, added_at_threshold, apply_last_watch_threshold_to_collections):
                 continue
             
             shows_needing_action.append(movie_data)
@@ -109,68 +129,69 @@ class Deleterr:
         logger.debug(f"Found {len(shows_needing_action)} movies needing action")
         return shows_needing_action
 
-def is_movie_actionable(library, activity_data, movie_data, plex_movie, last_watched_days, last_added_days):
-    # get the current time
-    now = datetime.now()
-
-    # Days since last watched
-    date_added = (now - plex_movie.addedAt).days
-    
-    if last_added_days and date_added < last_added_days:
-        logger.debug(f"Movie {movie_data['title']} added {date_added} days ago, skipping")
-        return False
-
-    # Exclusions
-    exclude = library.get('exclude', {})
-    if exclude:
-        for genre in exclude.get('genres', []):
-            if genre.lower() in (g.tag.lower() for g in plex_movie.genres):
-                logger.debug(f"Movie {movie_data['title']} has excluded genre {genre}, skipping")
+    def is_movie_actionable(self, library, activity_data, movie_data, plex_movie, last_watched_threshold, added_at_threshold, apply_last_watch_threshold_to_collections):          
+        watched_data = find_watched_data(movie_data, activity_data)
+        if watched_data:
+            last_watched = (datetime.now() - watched_data['last_watched']).days
+            if last_watched_threshold and last_watched < last_watched_threshold:
+                logger.debug(f"Movie {movie_data['title']} watched {last_watched} days ago, skipping")
                 return False
-
-        for collection in exclude.get('collections', []):
-            if collection.lower() in (g.tag.lower() for g in plex_movie.collections):
-                logger.debug(f"Movie {movie_data['title']} has excluded collection {collection}, skipping")
+            
+        if apply_last_watch_threshold_to_collections:
+            already_watched = self.watched_collections.intersection(set([c.tag for c in plex_movie.collections]))
+            if already_watched:
+                logger.debug(f"Movie {movie_data['title']} has watched collections ({already_watched}), skipping")
                 return False
-        
-        if exclude.get('release_years', 0):
-            if plex_movie.year >= datetime.now().year - exclude.get('release_years'):
-                logger.debug(f"Movie {movie_data['title']} ({plex_movie.year}) was released within the threshold years ({datetime.now().year} - {exclude.get('release_years', 0)} = {datetime.now().year - exclude.get('release_years', 0)}), skipping")
-                return False
-
-        for producer in exclude.get('producers', []):
-            if producer.lower() in (g.tag.lower() for g in plex_movie.producers):
-                logger.debug(f"Movie {movie_data['title']} has excluded producer {producer}, skipping")
-                return False
-        
-        for director in exclude.get('directors', []):
-            if director.lower() in (g.tag.lower() for g in plex_movie.directors):
-                logger.debug(f"Movie {movie_data['title']} has excluded director {director}, skipping")
-                return False
-
-        for writer in exclude.get('writers', []):
-            if writer.lower() in (g.tag.lower() for g in plex_movie.writers):
-                logger.debug(f"Movie {movie_data['title']} has excluded writer {writer}, skipping")
-                return False
-
-        for actor in exclude.get('actors', []):
-            if actor.lower() in (g.tag.lower() for g in plex_movie.roles):
-                logger.debug(f"Movie {movie_data['title']} has excluded actor {actor}, skipping")
-                return False
-
-        if plex_movie.studio and plex_movie.studio.lower() in exclude.get('studios', []):
-            logger.debug(f"Movie {movie_data['title']} has excluded studio {plex_movie.studio}, skipping")
+            
+        # Days since added
+        date_added = (datetime.now() - plex_movie.addedAt).days
+        if added_at_threshold and date_added < added_at_threshold:
+            logger.debug(f"Movie {movie_data['title']} added {date_added} days ago, skipping")
             return False
 
+        # Exclusions
+        exclude = library.get('exclude', {})
+        if exclude:
+            for genre in exclude.get('genres', []):
+                if genre.lower() in (g.tag.lower() for g in plex_movie.genres):
+                    logger.debug(f"Movie {movie_data['title']} has excluded genre {genre}, skipping")
+                    return False
 
-    watched_data = find_watched_data(movie_data, activity_data)
-    if watched_data:
-        last_watched = (now - watched_data['last_watched']).days
-        if last_watched_days and last_watched < last_watched_days:
-            logger.debug(f"Movie {movie_data['title']} watched {last_watched} days ago, skipping")
-            return False
-    
-    return True
+            for collection in exclude.get('collections', []):
+                if collection.lower() in (g.tag.lower() for g in plex_movie.collections):
+                    logger.debug(f"Movie {movie_data['title']} has excluded collection {collection}, skipping")
+                    return False
+                
+            if exclude.get('release_years', 0):
+                if plex_movie.year >= datetime.now().year - exclude.get('release_years'):
+                    logger.debug(f"Movie {movie_data['title']} ({plex_movie.year}) was released within the threshold years ({datetime.now().year} - {exclude.get('release_years', 0)} = {datetime.now().year - exclude.get('release_years', 0)}), skipping")
+                    return False
+
+            for producer in exclude.get('producers', []):
+                if producer.lower() in (g.tag.lower() for g in plex_movie.producers):
+                    logger.debug(f"Movie {movie_data['title']} has excluded producer {producer}, skipping")
+                    return False
+            
+            for director in exclude.get('directors', []):
+                if director.lower() in (g.tag.lower() for g in plex_movie.directors):
+                    logger.debug(f"Movie {movie_data['title']} has excluded director {director}, skipping")
+                    return False
+
+            for writer in exclude.get('writers', []):
+                if writer.lower() in (g.tag.lower() for g in plex_movie.writers):
+                    logger.debug(f"Movie {movie_data['title']} has excluded writer {writer}, skipping")
+                    return False
+
+            for actor in exclude.get('actors', []):
+                if actor.lower() in (g.tag.lower() for g in plex_movie.roles):
+                    logger.debug(f"Movie {movie_data['title']} has excluded actor {actor}, skipping")
+                    return False
+
+            if plex_movie.studio and plex_movie.studio.lower() in exclude.get('studios', []):
+                logger.debug(f"Movie {movie_data['title']} has excluded studio {plex_movie.studio}, skipping")
+                return False
+        
+        return True
 
 def find_watched_data(movie_data, activity_data):
     for watched_data in activity_data:
--- modules/tautulli.py
+++ modules/tautulli.py
@@ -2,10 +2,10 @@
 
 from tautulli import RawAPI
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 import logger
-
+import time
 
 def filter_by_most_recent(data, key, sort_key):
     # Create an empty dictionary to hold the highest stopped value for each id
@@ -32,29 +32,47 @@ class Tautulli:
             config.get("tautulli", "url"), config.get("tautulli", "api_key")
         )
 
-    def get_last_episode_activity(self):
-        return self.get_activity("episode")
+    def get_last_episode_activity(self, library_config, section):
+        return self.get_activity(section)
 
-    def get_last_movie_activity(self):
-        return self.get_activity("movie")
+    def get_last_movie_activity(self, library_config, section):
+        last_watched_threshold_date = datetime.now() - timedelta(days=library_config.get('last_watched_threshold'))
+        unwatched_threshold_date = datetime.now() - timedelta(days=library_config.get('added_at_threshold'))
+        min_date = min(last_watched_threshold_date, unwatched_threshold_date)
+
+        return self.get_activity(section, after=min_date, length=100)
 
     def refresh_library(self, section_id):
         self.api.get_library_media_info(section_id=section_id, refresh=True)
 
-    def get_activity(self, media_type, length=10):
-        # load the data
-        history = self.api.get_history(media_type=media_type, length=length)
+    def get_activity(self, section, **kwargs):
+
+        # Request params
+        start = 0
 
         # create a dictionary to store last watch activity for each show
         last_activity = []
+        raw_data = []
+
+        while True:
+            # load the data
+            history = self.api.get_history(section_id=section, order_column='date', order_direction="asc", start=start, **kwargs)    
+
+            if len(history["data"]) == 0:
+                break
+
+            start += len(history["data"])
+            raw_data += history["data"]
+
+            logger.debug("Got %s history items. next start: %s", len(history["data"]), start)
 
         key = (
             "grandparent_rating_key"
-            if history.get("data")[0].get("grandparent_rating_key", "") != ""
+            if raw_data[0].get("grandparent_rating_key", "") != ""
             else "rating_key"
         )
-
-        filtered_data = filter_by_most_recent(history["data"], key, "stopped")
+        
+        filtered_data = filter_by_most_recent(raw_data, key, "stopped")
         i = 0
         for entry in filtered_data:
             i += 1
