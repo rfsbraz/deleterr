0984ff04c61c53b5e3c25a2dac595156db410c49
Rodrigo Braz <rfsbraz@gmail.com>
2024-02-29 23:38:30 +0000

    chore: refactor logger

--- app/logger.py
+++ app/logger.py
@@ -34,6 +34,17 @@ def initLogger(console=False, log_dir=False, verbose=False):
     be invoked multiple times, during different stages of Deleterr.
     """
 
+    remove_old_handlers()
+    configure_logger(verbose)
+
+    if log_dir:
+        setup_file_logger(log_dir)
+
+    if console:
+        setup_console_logger(console)
+
+
+def remove_old_handlers():
     # Close and remove old handlers. This is required to reinit the loggers
     # at runtime
     log_handlers = logger.handlers[:]
@@ -46,45 +57,49 @@ def initLogger(console=False, log_dir=False, verbose=False):
 
         logger.removeHandler(handler)
 
+
+def configure_logger(verbose):
     # Configure the logger to accept all messages
     logger.propagate = False
     logger.setLevel(logging.DEBUG if verbose else logging.INFO)
 
+
+def setup_file_logger(log_dir):
     # Setup file logger
-    if log_dir:
-        file_formatter = logging.Formatter(
-            "%(asctime)s - %(levelname)-7s :: %(filename)s :: %(name)s : %(message)s",
-            "%Y-%m-%d %H:%M:%S",
-        )
+    file_formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)-7s :: %(filename)s :: %(name)s : %(message)s",
+        "%Y-%m-%d %H:%M:%S",
+    )
 
-        # Main logger
-        filename = os.path.join(log_dir, FILENAME)
-        file_handler = handlers.RotatingFileHandler(
-            filename, maxBytes=MAX_SIZE, backupCount=MAX_FILES, encoding="utf-8"
-        )
-        file_handler.setLevel(logging.DEBUG)
-        file_handler.setFormatter(file_formatter)
+    # Main logger
+    filename = os.path.join(log_dir, FILENAME)
+    file_handler = handlers.RotatingFileHandler(
+        filename, maxBytes=MAX_SIZE, backupCount=MAX_FILES, encoding="utf-8"
+    )
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(file_formatter)
 
-        logger.addHandler(file_handler)
+    logger.addHandler(file_handler)
 
+
+def setup_console_logger():
     # Setup console logger
-    if console:
-        console_formatter = logging.Formatter(
-            "%(asctime)s - %(levelname)s :: %(filename)s :: %(name)s : %(message)s",
-            "%Y-%m-%d %H:%M:%S",
-        )
+    console_formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s :: %(filename)s :: %(name)s : %(message)s",
+        "%Y-%m-%d %H:%M:%S",
+    )
 
-        stdout_handler = logging.StreamHandler(sys.stdout)
-        stdout_handler.setFormatter(console_formatter)
-        stdout_handler.setLevel(logging.DEBUG)
-        stdout_handler.addFilter(LogLevelFilter(logging.INFO))
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(console_formatter)
+    stdout_handler.setLevel(logging.DEBUG)
+    stdout_handler.addFilter(LogLevelFilter(logging.INFO))
 
-        stderr_handler = logging.StreamHandler(sys.stderr)
-        stderr_handler.setFormatter(console_formatter)
-        stderr_handler.setLevel(logging.WARNING)
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    stderr_handler.setFormatter(console_formatter)
+    stderr_handler.setLevel(logging.WARNING)
 
-        logger.addHandler(stdout_handler)
-        logger.addHandler(stderr_handler)
+    logger.addHandler(stdout_handler)
+    logger.addHandler(stderr_handler)
 
 
 # Expose logger methods
