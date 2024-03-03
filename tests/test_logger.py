e53278fbbd22f009811ff0a011868f6a87b4f237
Rodrigo Braz <rfsbraz@gmail.com>
2024-03-03 22:55:23 +0000

    chore: Add logger tests

---
+++ tests/test_logger.py
@@ -0,0 +1,23 @@
+import unittest
+from unittest.mock import patch, MagicMock
+from app import logger
+
+
+class TestLogger(unittest.TestCase):
+    @patch("app.logger.remove_old_handlers", autospec=True)
+    @patch("app.logger.configure_logger", autospec=True)
+    @patch("app.logger.setup_file_logger", autospec=True)
+    @patch("app.logger.setup_console_logger", autospec=True)
+    def test_initLogger(
+        self,
+        mock_setup_console_logger,
+        mock_setup_file_logger,
+        mock_configure_logger,
+        mock_remove_old_handlers,
+    ):
+        logger.initLogger(console=True, log_dir="log_dir", verbose=True)
+
+        mock_remove_old_handlers.assert_called_once()
+        mock_configure_logger.assert_called_once_with(True)
+        mock_setup_file_logger.assert_called_once_with("log_dir")
+        mock_setup_console_logger.assert_called_once()
