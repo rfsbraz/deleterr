933841265d7aba9869c962db4b7e377e0913a0fd
Rodrigo Braz <rfsbraz@gmail.com>
2024-03-13 22:06:44 +0000

    chore(refactor): increase deleterr coverage

--- tests/test_deleterr.py
+++ tests/test_deleterr.py
@@ -1,7 +1,15 @@
 import unittest
 from unittest.mock import MagicMock, patch
 
-from app.deleterr import main
+import pytest
+
+from app.deleterr import Deleterr, main
+
+
+@pytest.fixture
+def deleterr():
+    with patch("app.deleterr.MediaCleaner", return_value=MagicMock()):
+        yield Deleterr(MagicMock())
 
 
 class TestConfigPath(unittest.TestCase):
@@ -20,3 +28,53 @@ class TestConfigPath(unittest.TestCase):
         mock_load_config.assert_called_once_with("/config/settings.yaml")
         mock_config.validate.assert_called_once()
         mock_deleterr.assert_called_once_with(mock_config)
+
+
+@patch("app.deleterr.SonarrAPI")
+@patch("app.deleterr.RadarrAPI")
+def test_process_radarr(radarr_mock, sonarr_mock, deleterr):
+    # Arrange
+    deleterr.radarr = {
+        "Radarr1": MagicMock(
+            get_movie=MagicMock(return_value=[{"title": "Test Movie"}])
+        ),
+    }
+    deleterr.config.settings = {
+        "libraries": [{"radarr": "Radarr1"}],
+    }
+    deleterr.media_cleaner.process_library_movies = MagicMock(return_value=1000)
+
+    # Act
+    deleterr.process_radarr()
+
+    # Assert
+    deleterr.media_cleaner.process_library_movies.assert_called_once_with(
+        {"radarr": "Radarr1"},
+        deleterr.radarr["Radarr1"],
+        [{"title": "Test Movie"}],
+    )
+
+
+@patch("app.deleterr.SonarrAPI")
+@patch("app.deleterr.RadarrAPI")
+def test_process_sonarr(radarr_mock, sonarr_mock, deleterr):
+    # Arrange
+    deleterr.sonarr = {
+        "Sonarr1": MagicMock(
+            get_series=MagicMock(return_value=[{"title": "Test Movie"}])
+        ),
+    }
+    deleterr.config.settings = {
+        "libraries": [{"sonarr": "Sonarr1"}],
+    }
+    deleterr.media_cleaner.process_library = MagicMock(return_value=1000)
+
+    # Act
+    deleterr.process_sonarr()
+
+    # Assert
+    deleterr.media_cleaner.process_library.assert_called_once_with(
+        {"sonarr": "Sonarr1"},
+        deleterr.sonarr["Sonarr1"],
+        [{"title": "Test Movie"}],
+    )
