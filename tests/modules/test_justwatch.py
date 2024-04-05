9cc1ebd5fc2b2af500608b1812981643e60a786b
Rodrigo Braz <rfsbraz@gmail.com>
2024-04-05 16:55:33 +0100

    feat(justwatch): Add test coverage

--- tests/modules/test_justwatch.py
+++ tests/modules/test_justwatch.py
@@ -1,12 +1,12 @@
-import unittest
 from unittest.mock import MagicMock, patch
 
-from simplejustwatchapi.query import MediaEntry
+import pytest
 
 from app.modules.justwatch import JustWatch
 
 
-class TestJustWatch(unittest.TestCase):
+@pytest.mark.unit
+class TestJustWatch:
     @patch("app.modules.justwatch.search")
     def test_search(self, mock_search):
         # Arrange
@@ -17,8 +17,8 @@ class TestJustWatch(unittest.TestCase):
         result = justwatch_instance._search("test_title")
 
         # Assert
-        mock_search.assert_called_once_with("test_title", "US", "en", 5, True)
-        self.assertEqual(result, ["result1", "result2", "result3"])
+        mock_search.assert_called_once_with("test_title", "US", "en", 5, False)
+        assert result == ["result1", "result2", "result3"]
 
     @patch.object(JustWatch, "_search")
     def test_search_by_title_and_year(self, mock_search):
@@ -44,10 +44,7 @@ class TestJustWatch(unittest.TestCase):
 
         # Assert
         mock_search.assert_called_once_with("title1")
-        self.assertEqual(
-            result,
-            [mock_entry1],
-        )
+        assert result == mock_entry1
 
     @patch.object(JustWatch, "_search")
     def test_search_by_title_and_year(self, mock_search):
@@ -73,16 +70,16 @@ class TestJustWatch(unittest.TestCase):
 
         # Assert
         mock_search.assert_called_once_with("title1")
-        self.assertEqual(
-            result,
-            [mock_entry1, mock_entry3],
-        )
+        assert result == mock_entry1
 
     @patch.object(JustWatch, "search_by_title_and_year")
     def test_available_on(self, mock_search_by_title_and_year):
         # Arrange
+        mock_offer = MagicMock()
+        mock_offer.package.technical_name = "provider1"
+
         mock_entry1 = MagicMock()
-        mock_entry1.offers = ["provider1", "provider2"]
+        mock_entry1.offers = [mock_offer]
 
         mock_search_by_title_and_year.return_value = mock_entry1
         justwatch_instance = JustWatch("US", "en")
@@ -92,13 +89,16 @@ class TestJustWatch(unittest.TestCase):
 
         # Assert
         mock_search_by_title_and_year.assert_called_once_with("title1", 2001, "movie")
-        self.assertTrue(result)
+        assert result
 
     @patch.object(JustWatch, "search_by_title_and_year")
     def test_available_on_false(self, mock_search_by_title_and_year):
         # Arrange
+        mock_offer = MagicMock()
+        mock_offer.package.technical_name = "provider2"
+
         mock_entry1 = MagicMock()
-        mock_entry1.offers = ["provider2", "provider3"]
+        mock_entry1.offers = [mock_offer]
 
         mock_search_by_title_and_year.return_value = mock_entry1
         justwatch_instance = JustWatch("US", "en")
@@ -108,7 +108,7 @@ class TestJustWatch(unittest.TestCase):
 
         # Assert
         mock_search_by_title_and_year.assert_called_once_with("title1", 2001, "movie")
-        self.assertFalse(result)
+        assert not result
 
     @patch.object(JustWatch, "search_by_title_and_year")
     def test_available_on_any(self, mock_search_by_title_and_year):
@@ -124,7 +124,7 @@ class TestJustWatch(unittest.TestCase):
 
         # Assert
         mock_search_by_title_and_year.assert_called_once_with("title1", 2001, "movie")
-        self.assertTrue(result)
+        assert result
 
     @patch.object(JustWatch, "search_by_title_and_year")
     def test_available_on_no_result(self, mock_search_by_title_and_year):
@@ -137,7 +137,7 @@ class TestJustWatch(unittest.TestCase):
 
         # Assert
         mock_search_by_title_and_year.assert_called_once_with("title1", 2001, "movie")
-        self.assertFalse(result)
+        assert not result
 
     @patch.object(JustWatch, "available_on")
     def test_is_not_available_on_true(self, mock_available_on):
@@ -154,7 +154,7 @@ class TestJustWatch(unittest.TestCase):
         mock_available_on.assert_called_once_with(
             "title1", 2001, "movie", ["provider1"]
         )
-        self.assertTrue(result)
+        assert result
 
     @patch.object(JustWatch, "available_on")
     def test_is_not_available_on_false(self, mock_available_on):
@@ -171,4 +171,55 @@ class TestJustWatch(unittest.TestCase):
         mock_available_on.assert_called_once_with(
             "title1", 2001, "movie", ["provider1"]
         )
-        self.assertFalse(result)
+        assert not result
+
+
+@pytest.mark.integration
+class TestJustWatchIntegration:
+    def test_search(self):
+        # Arrange
+        justwatch_instance = JustWatch("US", "en")
+
+        # Act
+        result = justwatch_instance._search("Better Call Saul")
+
+        # Assert
+        assert isinstance(result, list)
+
+        assert result
+
+    def test_search_by_title_and_year(self):
+        # Arrange
+        justwatch_instance = JustWatch("US", "en")
+
+        # Act
+        result = justwatch_instance.search_by_title_and_year(
+            "Better Call Saul", 2015, "show"
+        )
+
+        # Assert that the object exists
+        assert result
+
+    def test_available_on(self):
+        # Arrange
+        justwatch_instance = JustWatch("US", "en")
+
+        # Act
+        result = justwatch_instance.available_on(
+            "Better Call Saul", 2015, "show", ["Netflix"]
+        )
+
+        # Assert
+        assert result
+
+    def test_is_not_available_on(self):
+        # Arrange
+        justwatch_instance = JustWatch("US", "en")
+
+        # Act
+        result = justwatch_instance.is_not_available_on(
+            "Better Call Saul", 2015, "show", ["Disney+"]
+        )
+
+        # Assert
+        assert result
