1a2f32052ba27292fdf0079c95f6bc1e633da251
Rodrigo Braz <rfsbraz@gmail.com>
2024-05-17 21:30:49 +0100

    chore: Remove useless test

--- tests/test_deleterr.py
+++ tests/test_deleterr.py
@@ -12,24 +12,6 @@ def deleterr():
         yield Deleterr(MagicMock())
 
 
-class TestConfigPath(unittest.TestCase):
-    @patch("app.deleterr.load_config")
-    @patch("app.deleterr.Deleterr")
-    @patch("app.deleterr.logger")
-    def test_default_config_path(self, mock_logger, mock_deleterr, mock_load_config):
-        # Arrange
-        mock_config = MagicMock()
-        mock_load_config.return_value = mock_config
-
-        # Act
-        main()
-
-        # Assert
-        mock_load_config.assert_called_once_with("/config/settings.yaml")
-        mock_config.validate.assert_called_once()
-        mock_deleterr.assert_called_once_with(mock_config)
-
-
 @patch("app.deleterr.SonarrAPI")
 @patch("app.deleterr.RadarrAPI")
 def test_process_radarr(radarr_mock, sonarr_mock, deleterr):
